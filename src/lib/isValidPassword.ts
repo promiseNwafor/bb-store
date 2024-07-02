e6a2952ed83b0bdec1db09ab2b63c9d7719155dd
promiseNwafor <promisenwafor955@gmail.com>
2024-07-02 08:43:54 +0100

    add pages

--- src/lib/isValidPassword.ts
+++ src/lib/isValidPassword.ts
@@ -9,7 +9,7 @@ async function hashPassword(password: string) {
   const arrayBuffer = await crypto.subtle.digest(
     "SHA-512",
     new TextEncoder().encode(password)
-  )
+  )  
 
   return Buffer.from(arrayBuffer).toString("base64")
 }
